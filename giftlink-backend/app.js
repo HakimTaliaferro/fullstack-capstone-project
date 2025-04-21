d1a38bc063acbb508f488a2b1d5bbe19e21db730
HakimTaliaferro <taliaferro.hakim@gmail.com>
2025-04-21 11:05:02 -0400

    keep going!

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,47 +1,57 @@
 /*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
+    require('dotenv').config();
+    const express = require('express');
+    const cors = require('cors');
+    const pinoLogger = require('./logger');
 
-const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
+    const connectToDatabase = require('./models/db');
+    const {loadData} = require("./util/import-mongo/index");
 
-// Import Routes
-const giftRoutes = require('./routes/giftRoutes');
-const searchRoutes = require('./routes/searchRoutes');
 
-const app = express();
-app.use("*", cors());
-const port = 3060;
+    const app = express();
+    app.use("*",cors());
+    const port = 3060;
 
-// Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
+    // Connect to MongoDB; we just do this one time
+    connectToDatabase().then(() => {
+        pinoLogger.info('Connected to DB');
+    })
+        .catch((e) => console.error('Failed to connect to DB', e));
 
-app.use(express.json());
 
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
+    app.use(express.json());
 
-app.use(pinoHttp({ logger }));
+    // Route files
+    // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
+    const giftRoutes = require('./routes/giftRoutes');
 
-// Use Routes
-app.use('/api/gifts', giftRoutes); // Task 2: Connect giftRoutes
-app.use('/api/search', searchRoutes); // Add search routes
+    // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+    //{{insert code here}}
 
-// Global Error Handler
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-});
 
-app.get("/", (req, res) => {
-    res.send("Inside the server");
-});
+    const pinoHttp = require('pino-http');
+    const logger = require('./logger');
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+    app.use(pinoHttp({ logger }));
+
+    // Use Routes
+    // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
+    app.use('/api/gifts', giftRoutes);
+
+    // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
+    //{{insert code here}}
+
+
+    // Global Error Handler
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    });
+
+    app.get("/",(req,res)=>{
+        res.send("Inside the server")
+    })
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
