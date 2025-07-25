44dd6b879006340a9808cb06c57605dbec43cb41
HakimTaliaferro <taliaferro.hakim@gmail.com>
2025-07-25 17:04:34 -0400

    Fixed imports

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 import './RegisterPage.css';
 
